3845fc2af04023799c4598d06ed6dd8f3f3db09f
wellighon <welldarlan.pw@gmail.com>
2023-10-07 22:09:49 -0300

    Atualização navbar

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function remove(){
     document.querySelector('#menu-btn').classList.remove('fa-times')
     document.querySelector('.navbar').classList.remove('active')
 }
-// ------------------EFEITO write------------------
+// ------------------EFEITO WRITE------------------
 
 function typeWriter(element) {
     const textArray = element.innerHTML.split('');
@@ -47,6 +47,8 @@ function isElementInViewport(element) {
 
 const aboutP = document.getElementById("description");
 
+
+//Função será acionada assim que o elemento estiver visivel
 function startTypeWriterWhenVisible() {
     if (isElementInViewport(aboutP)) {
         typeWriter(aboutP);
@@ -146,165 +148,10 @@ closeYodaSMS.addEventListener("click", function(){
 })
 
 
-//efeito background
 
-const canvas = document.querySelector("#canvas");
-const ctx = canvas.getContext("2d");
-
-let w, h, particles;
-let particleDistance = 40;
-let mouse = {
-	x: undefined,
-	y: undefined,
-	radius: 100
-}
-
-function init() {
-	resizeReset();
-	animationLoop();
-}
-
-function resizeReset() {
-	w = canvas.width = window.innerWidth;
-	h = canvas.height = window.innerHeight;
-
-	particles = [];
-	for (let y = (((h - particleDistance) % particleDistance) + particleDistance) / 2; y < h; y += particleDistance) {
-		for (let x = (((w - particleDistance) % particleDistance) + particleDistance) / 2; x < w; x += particleDistance) {
-			particles.push(new Particle(x, y));
-		}
-	}
-}
-
-function animationLoop() {
-	ctx.clearRect(0, 0, w, h);
-	drawScene();
-	requestAnimationFrame(animationLoop);
-}
-
-function drawScene() {
-	for (let i = 0; i < particles.length; i++) {
-		particles[i].update();
-		particles[i].draw();
-	}
-	drawLine();
-}
-
-function drawLine() {
-	for (let a = 0; a < particles.length; a++) {
-		for (let b = a; b < particles.length; b++) {
-			let dx = particles[a].x - particles[b].x;
-			let dy = particles[a].y - particles[b].y;
-			let distance = Math.sqrt(dx * dx + dy * dy);
-
-			if (distance < particleDistance * 1.5) {
-				opacity = 1 - (distance / (particleDistance * 1.5));
-				ctx.strokeStyle = "rgba(10,10,10," + opacity + ")";
-				ctx.lineWidth = 2;
-				ctx.beginPath();
-				ctx.moveTo(particles[a].x, particles[a].y);
-				ctx.lineTo(particles[b].x, particles[b].y);
-				ctx.stroke();
-			}
-		}
-	}
-}
-
-function mousemove(e) {
-	mouse.x = e.x;
-	mouse.y = e.y;
-}
-
-function mouseout() {
-	mouse.x = undefined;
-	mouse.y = undefined;
-}
-
-class Particle {
-	constructor(x, y) {
-		this.x = x;
-		this.y = y;
-		this.size = 4;
-		this.baseX = this.x;
-		this.baseY = this.y;
-		this.speed = (Math.random() * 25) + 5;
-	}
-	draw() {
-		ctx.fillStyle = "rgba(37,41,52,1)";
-		ctx.beginPath();
-		ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-		ctx.closePath();
-		ctx.fill();
-	}
-	update() {
-		let dx = mouse.x - this.x;
-		let dy = mouse.y - this.y;
-		let distance = Math.sqrt(dx * dx + dy * dy);
-		let maxDistance = mouse.radius;
-		let force = (maxDistance - distance) / maxDistance; // 0 ~ 1
-		let forceDirectionX = dx / distance;
-		let forceDirectionY = dy / distance;
-		let directionX = forceDirectionX * force * this.speed;
-		let directionY = forceDirectionY * force * this.speed;
-
-		if (distance < mouse.radius) {
-			this.x -= directionX;
-			this.y -= directionY;
-		} else {
-			if (this.x !== this.baseX) {
-				let dx = this.x - this.baseX;
-				this.x -= dx / 10;
-			}
-			if (this.y !== this.baseY) {
-				let dy = this.y - this.baseY;
-				this.y -= dy / 10;
-			}
-		}
-	}
-}
-
-init();
-window.addEventListener("resize", resizeReset);
-window.addEventListener("mousemove", mousemove);
-window.addEventListener("mouseout", mouseout);
 
 // -----------------BTN START
 const ViewMyWork = document.getElementById("ViewMyWork")
 
 
-// ViewMyWork.addEventListener("click", function() {
-//     canvas.style.display = "none"
-// })
-// efeito maquina de escrever no sobre
 
-
-// function typeWriter (element) {
-//     const textArray = element.innerHTML.split('')
-//     element.innerHTML = ''
-//     textArray.forEach((letra, i) => {
-//     setTimeout(() => element.innerHTML += letra, 30 * i)
-//     })
-// }
-
-
-// const aboutP = document.getElementById("description")
-// typeWriter(aboutP)
-
-
-
-
-
-
-// const project = document.getElementById("projects")
-// const project = document.getElementById("projects")
-// const btnVerMais = document.getElementById("btnVerMais")
-
-//     function verMais() {
-//         if (project.style.height < "1300px"){
-//         project.style.height = "3000px"
-//         btnVerMais.innerText = "VER MENOS"
-//     }else {
-//         project.style.height = "1200px"
-//         btnVerMais.innerText = "VER MAIS"
-//     }
-// }
